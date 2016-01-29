634ac6c3635b0075b5ec894315f9fab38840cee9
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-01-29 14:24:27 -0500

    Updated from Brython Server: 1/29/2016 2:24:42 PM

--- calculatepi.py
+++ calculatepi.py
@@ -1,7 +1,7 @@
 """
 calculatepi.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Mary Feyrer
+Credit: Tess Snyder, Mr. Dennison 
 Assignment:
 
 Write and submit a Python program that computes an approximate value of π by calculating the following sum:
